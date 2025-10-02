a3d427a1690dbcfbcbcea5364c786ec6be85a0dc
Piotr Rodak <piotr.rodak@maycove.com>
2025-10-02 22:47:36 +0100

    added Lucene vector database

---
+++ src/main/java/com/sample/agenttools/services/operation/LucenePromptVectorService.java
@@ -0,0 +1,59 @@
+package com.sample.agenttools.services.operation;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LucenePromptVectorService {
+    private static final String INDEX_DIR = "data/lucene-index";
+    private static final String FIELD_ID = "id";
+    private static final String FIELD_DESCRIPTION = "description";
+    private static final String FIELD_CONTENT = "content";
+    private static final String FIELD_VECTOR = "embedding";
+
+    private final FSDirectory directory;
+    private final StandardAnalyzer analyzer;
+
+    public LucenePromptVectorService() throws IOException {
+        this.directory = FSDirectory.open(Paths.get(INDEX_DIR));
+        this.analyzer = new StandardAnalyzer();
+    }
+
+    public void addPrompt(String id, String description, String content, float[] embedding) throws IOException {
+        try (IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(analyzer))) {
+            Document doc = new Document();
+            doc.add(new StringField(FIELD_ID, id, Field.Store.YES));
+            doc.add(new TextField(FIELD_DESCRIPTION, description, Field.Store.YES));
+            doc.add(new TextField(FIELD_CONTENT, content, Field.Store.YES));
+            doc.add(new KnnVectorField(FIELD_VECTOR, embedding));
+            writer.updateDocument(new Term(FIELD_ID, id), doc);
+        }
+    }
+
+    public List<Document> searchSimilarPrompts(float[] queryEmbedding, int k) throws IOException {
+        try (DirectoryReader reader = DirectoryReader.open(directory)) {
+            IndexSearcher searcher = new IndexSearcher(reader);
+            KnnVectorQuery query = new KnnVectorQuery(FIELD_VECTOR, queryEmbedding, k);
+            TopDocs topDocs = searcher.search(query, k);
+            List<Document> results = new ArrayList<>();
+            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                results.add(searcher.doc(scoreDoc.doc));
+            }
+            return results;
+        }
+    }
+
+    public void deletePrompt(String id) throws IOException {
+        try (IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(analyzer))) {
+            writer.deleteDocuments(new Term(FIELD_ID, id));
+        }
+    }
+}
